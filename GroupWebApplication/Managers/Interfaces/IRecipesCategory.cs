768b2d788d73ee48d210df97b4752f87d794b946
Руслан Хакимов <Stud118292@stankin.ru>
2022-04-22 18:57:23 +0300

    Начало работы Созданы интерфейсы, контроллеры, модели

---
+++ GroupWebApplication/Managers/Interfaces/IRecipesCategory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GroupWebApplication.Models;
+
+namespace GroupWebApplication.Managers.Interfaces
+{
+    public interface IRecipesCategory
+    {
+        IEnumerable<Category> AllCategories { get; }
+    }
+}
---
+++ GroupWebApplication/Managers/Mocks/MockRecipes.cs
@@ -0,0 +1,29 @@
+﻿using GroupWebApplication.Managers.Interfaces;
+using GroupWebApplication.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GroupWebApplication.Managers.Mocks
+{
+    public class MockRecipes : IAllRecipes
+    {
+        private readonly IRecipesCategory _categoryReceipts = new MockCategory();
+
+        public IEnumerable<Recipe> Recipes {
+            get
+            {
+                return new List<Recipe>
+                {
+                    new Recipe { name = "Айсберг", shortDesc = "", longDesc="", img = "",IsFavourite = true, Category = _categoryReceipts.AllCategories.First() },
+                };
+
+            }
+        }
+        public IEnumerable<Recipe> getFavRecipes { get ; set ; }
+
+        public Recipe getObjectRecipe(int recipeid)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
